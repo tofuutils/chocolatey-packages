f1e2df820a46f7d6b766c2e3c7522f18f00a970a
tofuutils <tofuutils@github.com>
2024-06-06 16:25:47 +0000

    Chocolatey scripts update for tenv version v1.11.19

--- tenv/tools/chocolateyinstall.ps1
+++ tenv/tools/chocolateyinstall.ps1
@@ -1,9 +1,9 @@
 $ErrorActionPreference = 'Stop'
 
-$url = 'https://github.com/tofuutils/tenv/releases/download/v1.11.8/tenv_v1.11.8_Windows_i386.zip'
-$url64 = 'https://github.com/tofuutils/tenv/releases/download/v1.11.8/tenv_v1.11.8_Windows_x86_64.zip'
-$checksum = '53cfe13bd547ad83dec6ed2a9ebc023997d1fff86045b03ecf4bd5ea6516d18d'
-$checksum64 = '410df43fde351163630a054a5a6ec2c8a708df42ab36ac383496e28fe468d820'
+$url = 'https://github.com/tofuutils/tenv/releases/download/v1.11.19/tenv_v1.11.19_Windows_i386.zip'
+$url64 = 'https://github.com/tofuutils/tenv/releases/download/v1.11.19/tenv_v1.11.19_Windows_x86_64.zip'
+$checksum = 'd191a6b6b7135239301be0a3c6991685150c9f2911321adc1b461189c99c424b'
+$checksum64 = '04d6cab3d6d6902c4beda8ac439eccab4879ca735a4c73fdef25648e838980f2'
 
 $unzipLocation = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
